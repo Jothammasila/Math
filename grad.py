ab771011f550be80fb4ce38393c7596d44d74cd1
Jothammasila <jothammasila@gmail.com>
2024-01-31 00:39:16 +0300

    Added the gradient class, that is the grad.py. This is used to find the gradient of any single co-efficient function

---
+++ grad.py
@@ -0,0 +1,9 @@
+class Grad:
+    def __init__(self, a: float, x: float, n: float=1) -> float:
+        self.a = a
+        self.x = x
+        self.n = n
+        
+    def grad(self,k):
+        return self.a*(self.n*(self.x**(self.n-1)))
+
